a385c8969a4026c363c7ebf6223b7af85c163d0a
wangerya <1454755870@qq.com>
2021-05-11 15:10:20 +0800

    add pages

---
+++ page.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+module.exports = [
+  {
+    name: 'index',
+    html: 'home/home.html',
+    jsEntry: 'home/index.js'
+  },
+  {
+    name: 'about',
+    html: 'about/about.html',
+    jsEntry: 'about/about.js'
+  },
+  {
+    name: 'children',
+    html: 'children/children.html',
+    jsEntry: 'children/children.js'
+  },
+  {
+    name: 'app',
+    html: 'app/app.html',
+    jsEntry: 'app/app.js'
+  }
+]
---
+++ src/pages/about/about.js
@@ -0,0 +1,11 @@
+import '../../common/css/com.less';
+// import $ from 'jquery';
+
+// window.$ = $
+// window.jQuery = $
+
+// $(document).ready(function () {
+//   $('.tab>div').click(function () {
+//     window.location.href('./about.html');
+//   });
+// });
