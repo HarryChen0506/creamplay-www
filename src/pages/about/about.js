55a58015f579fd443f991d1e567ad3671be938b3
wangerya <1454755870@qq.com>
2021-05-16 12:48:35 +0800

    mobile terminal adaptation

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,11 +1,38 @@
 import '../../common/css/com.less';
-// import $ from 'jquery';
+import $ from 'jquery';
+import menu from '../../images/button.png';
+import logo from '../../images/logo-m.png';
+import home from '../../images/home-m.png';
+import children from '../../images/children-m.png';
+import app from '../../images/app-m.png';
+import about from '../../images/about-m.png';
 
-// window.$ = $
-// window.jQuery = $
+window.$ = $
+window.jQuery = $
 
-// $(document).ready(function () {
-//   $('.tab>div').click(function () {
-//     window.location.href('./about.html');
-//   });
-// });
+function IsApp () {
+  var userAgent = navigator.userAgent;
+  var Agents = ['Android', 'iPhone', 'SymbianOS', 'Windows Phone', 'iPad', 'iPod'];
+  console.log('userAgent:', userAgent)
+  return Agents.some((i) => {
+    return userAgent.includes(i)
+  })
+}
+
+$(document).ready(function () {
+  var isApp = IsApp();
+  var homeContent = '<div class="imgContent"><img id="home" src="' + home + '" alt="home"><img id="children" src="' + children + '" alt="children"><img id="app" src="' + app + '" alt="app"><img id="about" src="' + about + '" alt="about"></div>';
+  var menuContent = '<div class="nav" style="display:none;"><ul><li><a href="#home">首页</a></li><li><a href="#children">儿童个护</a></li><li><a href="#app">稚造APP</a></li><li><a href="#about">关于我们</a></li></ul></div>';
+  if (isApp) {
+    $('.header').html('<img class="menu" src="' + menu + '" alt="home"><img class="logo" src="' + logo + '" alt="home">');
+    $('.content').html(menuContent + homeContent);
+  }
+  $('.menu').click(function () {
+    $('.nav').show();
+    $('.imgContent').hide();
+  });
+  $('.nav li').click(function () {
+    $('.nav').hide();
+    $('.imgContent').show();
+  });
+});
