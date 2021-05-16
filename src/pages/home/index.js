e3e974299d8b7c28dad3aa3bc4c4ae14a360389c
wangerya <1454755870@qq.com>
2021-05-16 13:08:12 +0800

    mobile terminal adaptation

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -13,7 +13,6 @@ window.jQuery = $
 function IsApp () {
   var userAgent = navigator.userAgent;
   var Agents = ['Android', 'iPhone', 'SymbianOS', 'Windows Phone', 'iPad', 'iPod'];
-  console.log('userAgent:', userAgent)
   return Agents.some((i) => {
     return userAgent.includes(i)
   })
@@ -22,7 +21,7 @@ function IsApp () {
 $(document).ready(function () {
   var isApp = IsApp();
   var homeContent = '<div class="imgContent"><img id="home" src="' + home + '" alt="home"><img id="children" src="' + children + '" alt="children"><img id="app" src="' + app + '" alt="app"><img id="about" src="' + about + '" alt="about"></div>';
-  var menuContent = '<div class="nav" style="display:none;"><ul><li><a href="#home">首页</a></li><li><a href="#children">儿童个护</a></li><li><a href="#app">稚造APP</a></li><li><a href="#about">关于我们</a></li></ul></div>';
+  var menuContent = '<div class="nav" style="display:none;"><ul><li name="首页"><a href="#home">首页</a></li><li name="儿童个护"><a href="#children">儿童个护</a></li><li name="稚造APP"><a href="#app">稚造APP</a></li><li name="关于我们"><a href="#about">关于我们</a></li></ul></div>';
   if (isApp) {
     $('.header').html('<img class="menu" src="' + menu + '" alt="home"><img class="logo" src="' + logo + '" alt="home">');
     $('.content').html(menuContent + homeContent);
@@ -33,6 +32,7 @@ $(document).ready(function () {
   });
   $('.nav li').click(function () {
     $('.nav').hide();
+    document.title = $(this).attr('name');
     $('.imgContent').show();
   });
 });
