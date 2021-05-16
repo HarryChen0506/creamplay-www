e1f4116d43593df23cc5bb8a16a62b8003273afa
wangerya <1454755870@qq.com>
2021-05-16 16:30:34 +0800

    change code

--- src/pages/app/app.js
+++ src/pages/app/app.js
@@ -1,11 +1,14 @@
 import '../../common/css/com.less';
 import $ from 'jquery';
-import menu from '../../images/button.png';
-import logo from '../../images/logo-m.png';
-import home from '../../images/home-m.png';
-import children from '../../images/children-m.png';
-import app from '../../images/app-m.png';
-import about from '../../images/about-m.png';
+import menuM from '../../images/button.png';
+import selected from '../../images/tab-selected.png';
+import logo from '../../images/logo@2x.png';
+import app from '../../images/app.png';
+import logoM from '../../images/logo-m.png';
+import homeM from '../../images/home-m.png';
+import childrenM from '../../images/children-m.png';
+import appM from '../../images/app-m.png';
+import aboutM from '../../images/about-m.png';
 
 window.$ = $
 window.jQuery = $
@@ -18,22 +21,64 @@ function IsApp () {
   })
 }
 
-$(document).ready(function () {
-  var isApp = IsApp();
-  var homeContent = '<div class="imgContent"><img id="home" src="' + home + '" alt="home"><img id="children" src="' + children + '" alt="children"><img id="app" src="' + app + '" alt="app"><img id="about" src="' + about + '" alt="about"></div>';
-  var menuContent = '<div class="nav" style="display:none;"><ul><li name="首页"><a href="#home">首页</a></li><li name="儿童个护"><a href="#children">儿童个护</a></li><li name="稚造APP"><a href="#app">稚造APP</a></li><li name="关于我们"><a href="#about">关于我们</a></li></ul></div>';
-  if (isApp) {
-    document.title = 'CreamPlay';
-    $('.header').html('<img class="menu" src="' + menu + '" alt="home"><img class="logo" src="' + logo + '" alt="home">');
-    $('.content').html(menuContent + homeContent);
-  }
+function dealApp () {
+  var homeContent = '<div class="imgContent"><img id="home" src="' + homeM + '" alt="home" /><img id="children" src="' + childrenM + '" alt="children" /><img id="app" src="' + appM + '" alt="app"><img id="about" src="' + aboutM + '" alt="about" /></div>';
+  var menuContent = '<div class="nav hide"><ul><li name="首页"><a href="#home">首页</a></li><li name="儿童个护"><a href="#children">儿童个护</a></li><li name="稚造APP"><a href="#app">稚造APP</a></li><li name="关于我们"><a href="#about">关于我们</a></li></ul></div>';
+  document.title = 'CreamPlay';
+  $('.header').html('<img class="menu" src="' + menuM + '" alt="home" /><img class="logo" src="' + logoM + '" alt="logo" />');
+  $('.main').addClass('app');
+  $('.content').html(menuContent + homeContent);
   $('.menu').click(function () {
-    $('.nav').show();
-    $('.imgContent').hide();
+    if ($('.nav').hasClass('show')) {
+      $('.nav').removeClass('show').addClass('hide');
+      $('.imgContent').removeClass('hide').addClass('show');
+    } else {
+      $('.nav').removeClass('hide').addClass('show');
+      $('.imgContent').removeClass('show').addClass('hide');
+    }
   });
   $('.nav li').click(function () {
-    $('.nav').hide();
+    $('.nav').removeClass('show').addClass('hide');
     // document.title = $(this).attr('name');
-    $('.imgContent').show();
+    $('.imgContent').removeClass('hide').addClass('show');
   });
+}
+function dealWeb () {
+  var headerContent = '<img src="' + logo + '" alt="logo" class="logo" />' +
+  '<div class="tab">' +
+    '<div class="home">' +
+      '<a href="./">首页</a><img src="' + selected + '" alt="selected" />' +
+   '</div>' +
+    '<div class="children">' +
+     '<a href="./children.html">儿童个护</a><img src="' + selected + '" alt="selected" />' +
+    '</div>' +
+   '<div class="app selected">' +
+      '<a href="#">稚造APP</a><img src="' + selected + '" alt="selected" />' +
+    '</div>' +
+    '<div class="about">' +
+      '<a href="./about.html">关于我们</a><img src="' + selected + '" alt="selected" />' +
+    '</div>' +
+  '</div>';
+  var content = '<img src="' + app + '" alt="app" />';
+  $('.main').removeClass('app');
+  $('.header').html(headerContent);
+  $('.content').html(content);
+}
+
+$(document).ready(function () {
+  $(window).resize(function () {
+    var width = $(window).width();
+    if (width < 768) {
+      dealApp();
+    } else {
+      dealWeb()
+    }
+  });
+  var isApp = IsApp();
+  var width = $(window).width();
+  if (isApp || width < 768) {
+    dealApp()
+  } else {
+    dealWeb()
+  }
 });
