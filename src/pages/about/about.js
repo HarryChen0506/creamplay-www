cd94bb9e5d7c02a2ad94cdf2f8747c56b9cebadf
wangerya <1454755870@qq.com>
2021-05-25 14:38:01 +0800

    add copyright

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -9,6 +9,7 @@ import homeM from '../../images/home-m.png';
 import childrenM from '../../images/children-m.png';
 import appM from '../../images/app-m.png';
 import aboutM from '../../images/about-m.png';
+import police from '../../images/police.png';
 
 window.$ = $
 window.jQuery = $
@@ -22,7 +23,15 @@ function IsApp () {
 }
 
 function dealApp () {
-  var homeContent = '<div class="imgContent"><img id="home" src="' + homeM + '" alt="home" /><img id="children" src="' + childrenM + '" alt="children" /><img id="app" src="' + appM + '" alt="app" /><img id="about" src="' + aboutM + '" alt="about" /></div>';
+  var homeContent = '<div class="imgContent"><img id="home" src="' + homeM + '" alt="home" /><img id="children" src="' + childrenM + '" alt="children" /><img id="app" src="' + appM + '" alt="app" /><img id="about" src="' + aboutM + '" alt="about" />' +
+    '<div class="footer">' +
+    '<div class="footer-border"></div>' +
+    '<div class="copyright">Copyright © 2021 Creamplay 保留所有权利。</div>' +
+    '<div class="hrefs">' +
+    '<a href="https://beian.miit.gov.cn/#/Integrated/index">沪ICP备2021000596号-1</a> ｜ <img src="' + police + '" alt="">' +
+    '<a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31011002005418">沪公安网备31011002005418号</a>' +
+    '</div></div>' +
+    '</div>';
   var menuContent = '<div class="nav hide"><ul><li name="首页"><a href="#home">首页</a></li><li name="儿童个护"><a href="#children">儿童个护</a></li><li name="稚造APP"><a href="#app">稚造APP</a></li><li name="关于我们"><a href="#about">关于我们</a></li></ul></div>';
   document.title = 'CreamPlay';
   $('.header').html('<img class="menu" src="' + menuM + '" alt="home" /><img class="logo" src="' + logoM + '" alt="logo" />');
@@ -45,21 +54,28 @@ function dealApp () {
 }
 function dealWeb () {
   var headerContent = '<img src="' + logo + '" alt="logo" class="logo" />' +
-  '<div class="tab">' +
+    '<div class="tab">' +
     '<div class="home">' +
-      '<a href="./">首页</a><img src="' + selected + '" alt="selected" />' +
-   '</div>' +
-    '<div class="children">' +
-     '<a href="./children.html">儿童个护</a><img src="' + selected + '" alt="selected" />' +
+    '<a href="./">首页</a><img src="' + selected + '" alt="selected" />' +
     '</div>' +
-   '<div class="app">' +
-      '<a href="./app.html">稚造APP</a><img src="' + selected + '" alt="selected" />' +
+    '<div class="children">' +
+    '<a href="./children.html">儿童个护</a><img src="' + selected + '" alt="selected" />' +
+    '</div>' +
+    '<div class="app">' +
+    '<a href="./app.html">稚造APP</a><img src="' + selected + '" alt="selected" />' +
     '</div>' +
     '<div class="about selected">' +
-      '<a href="#">关于我们</a><img src="' + selected + '" alt="selected" />' +
+    '<a href="#">关于我们</a><img src="' + selected + '" alt="selected" />' +
     '</div>' +
-  '</div>';
-  var content = '<img src="' + about + '" alt="about" />';
+    '</div>';
+  var content = '<img src="' + about + '" alt="about" />' +
+    '<div class="footer">' +
+    '<div class="footer-border"></div>' +
+    '<div class="copyright">Copyright © 2021 Creamplay 保留所有权利。</div>' +
+    '<div class="hrefs">' +
+    '<a href="https://beian.miit.gov.cn/#/Integrated/index">沪ICP备2021000596号-1</a> ｜ <img src="' + police + '" alt="">' +
+    '<a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31011002005418">沪公安网备31011002005418号</a>' +
+    '</div></div>';
   $('.main').removeClass('app');
   $('.header').html(headerContent);
   $('.content').html(content);
